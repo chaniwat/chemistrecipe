4949f0a2168dd5563344991c169ff2f4db99b4b6
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2016-12-22 17:24:00 +0700

    test multi-target markers

---
+++ Assets/Resources/Scripts/FillableObject.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chemistrecipe
+{
+    public class FillableObject : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (transform.forward.y < -0.2f)
+            {
+                Debug.Log(gameObject.name + " is Facing down " + transform.forward);
+            }
+        }
+
+    }
+}
--- Assets/Resources/Scripts/TrackingImage.cs
+++ Assets/Resources/Scripts/TrackingImage.cs
@@ -4,82 +4,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class TrackingImage : MonoBehaviour, ITrackableEventHandler {
-
-    // Models path
-    public const string MODEL_PATH = "Models/";
-    // Models load
-    private Dictionary<string, UnityEngine.Object> models = new Dictionary<string, UnityEngine.Object>();
-
-    // Tracker
-    private TrackableBehaviour mTrackableBehaviour;
-    // State
-    private bool flag = true;
-
-    // Initialize
-    void Start () {
-        // Get and register image tracker
-        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-        if(mTrackableBehaviour)
-        {
-            mTrackableBehaviour.RegisterTrackableEventHandler(this);
-        }
-
-        // Load models
-        models["m1"] = Resources.Load(MODEL_PATH + "m1") as UnityEngine.Object;
-        models["m2"] = Resources.Load(MODEL_PATH + "m2") as UnityEngine.Object;
-
-        Debug.Log(models["m1"]);
-
-        // Attach default child
-        GameObject newObj = (GameObject)Instantiate(models["m1"], transform);
-        newObj.transform.localPosition = new Vector3(0, 0, 0);
-        newObj.transform.localEulerAngles = new Vector3(-90f, 0, 0);
-        newObj.transform.localScale = new Vector3(0.00650444f, 0.00650444f, 0.00650444f);
-    }
-
-    // Tracker handler (Observer)
-    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+namespace chemistrecipe
+{
+    public class TrackingImage : MonoBehaviour, ITrackableEventHandler
     {
-        if(newStatus == TrackableBehaviour.Status.DETECTED ||
-                newStatus == TrackableBehaviour.Status.TRACKED ||
-                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-        {
-            Debug.Log("detected");
-        }
-        else
-        {
-            Debug.Log("lost");
-        }
-    }
 
-    // Swapping model
-    public void swapModel()
-    {
-        // Remove all child
-        List<GameObject> children = new List<GameObject>();
-        foreach(Transform child in transform)
-        {
-            children.Add(child.gameObject);
-        }
-        children.ForEach(child => Destroy(child));
+        // Models path
+        public const string OBJECT_PATH = "Objects/";
+        // Models load
+        private Dictionary<string, UnityEngine.Object> models = new Dictionary<string, UnityEngine.Object>();
 
-        if (flag)
+        // Tracker
+        private TrackableBehaviour mTrackableBehaviour;
+        // Status
+        private TrackableBehaviour.Status tStatus;
+        // State
+        private bool flag = true;
+
+        // Initialize
+        void Start()
         {
-            GameObject newObj = (GameObject) Instantiate(models["m2"], transform);
+            // Get and register image tracker
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+
+            // Load models
+            models["musk"] = Resources.Load(OBJECT_PATH + "musk") as UnityEngine.Object;
+            models["beaker"] = Resources.Load(OBJECT_PATH + "beaker") as UnityEngine.Object;
+
+            // Attach default child
+            GameObject newObj = (GameObject)Instantiate(models["musk"], transform);
             newObj.transform.localPosition = new Vector3(0, 0, 0);
             newObj.transform.localEulerAngles = new Vector3(-90f, 0, 0);
-            newObj.transform.localScale = new Vector3(0.005766949f, 0.005766949f, 0.005766949f);
+            newObj.transform.localScale = new Vector3(0.00650444f, 0.00650444f, 0.00650444f);
         }
-        else
+
+        // Tracker handler (Observer)
+        public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
         {
-            GameObject newObj = (GameObject)Instantiate(models["m1"], transform);
+            tStatus = newStatus;
+
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                    newStatus == TrackableBehaviour.Status.TRACKED ||
+                    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                Debug.Log("detected");
+            }
+            else
+            {
+                Debug.Log("lost");
+            }
+        }
+
+        // Swapping model
+        public void swapModel()
+        {
+            // Remove all child
+            List<GameObject> children = new List<GameObject>();
+            foreach (Transform child in transform)
+            {
+                children.Add(child.gameObject);
+            }
+            children.ForEach(child => Destroy(child));
+
+            // Create new model
+            GameObject newObj;
+            if (flag)
+            {
+                newObj = (GameObject)Instantiate(models["beaker"], transform);
+                newObj.transform.localScale = new Vector3(0.005766949f, 0.005766949f, 0.005766949f);
+            }
+            else
+            {
+                newObj = (GameObject)Instantiate(models["musk"], transform);
+                newObj.transform.localScale = new Vector3(0.00650444f, 0.00650444f, 0.00650444f);
+            }
+
             newObj.transform.localPosition = new Vector3(0, 0, 0);
             newObj.transform.localEulerAngles = new Vector3(-90f, 0, 0);
-            newObj.transform.localScale = new Vector3(0.00650444f, 0.00650444f, 0.00650444f); 
+
+            // Hide if can't detect marker
+            if (tStatus == TrackableBehaviour.Status.NOT_FOUND ||
+                    tStatus == TrackableBehaviour.Status.UNDEFINED ||
+                    tStatus == TrackableBehaviour.Status.UNKNOWN)
+            {
+                Renderer[] rendererComponents = newObj.GetComponentsInChildren<Renderer>(true);
+                Collider[] colliderComponents = newObj.GetComponentsInChildren<Collider>(true);
+
+                // Disable rendering
+                foreach (Renderer component in rendererComponents)
+                {
+                    component.enabled = false;
+                }
+
+                // Disable colliders
+                foreach (Collider component in colliderComponents)
+                {
+                    component.enabled = false;
+                }
+            }
+
+            flag = !flag;
         }
 
-        flag = !flag;
     }
-
 }
