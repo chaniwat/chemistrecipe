14d413af518deb273ea15b367b6b0034c2f6a634
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-01-24 15:44:45 +0700

    add JSON.NET, fix all of Main Menu Scene

---
+++ Assets/Resources/Scripts/Data/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chemistrecipe.data
+{
+    public class Course
+    {
+        public string id;
+        public string name;
+        public string description;
+
+        public string assetBundlePath;
+        public string markerBundlePath;
+    }
+}
