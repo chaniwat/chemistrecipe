191679ff70aa4f23acc847047713273156ea72ec
Marktrs <marktrs69@gmail.com>
2017-02-02 03:35:41 +0700

    Create finale animate scene and control script

---
+++ Assets/Resources/Scripts/Animation/FinaleTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinaleTrigger : MonoBehaviour {
+
+    public Animator beaker;
+    public Animator soap1;
+    public Animator soap2;
+
+	// Use this for initialization
+	void Start () {
+        StopAnimation();
+
+    }
+
+    public void PlayAnimation() {
+        beaker.enabled = true;
+        soap1.enabled = true;
+        soap2.enabled = true;
+    }
+
+    public void StopAnimation() {
+        beaker.enabled = false;
+        soap1.enabled = false;
+        soap2.enabled = false;
+    }
+
+
+}
