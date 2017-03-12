2b32ba9e6e1ef5daffacd7d61f2f2f3113b72485
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2017-03-12 23:26:10 +0700

    Prepare localization

---
+++ Assets/Resources/Scripts/Localization/LocalLanguage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chemistrecipe.localization
+{
+    public abstract class LocalLanguage
+    {
+
+        private string _local_code;
+        public string local_code
+        {
+            get
+            {
+                return _local_code;
+            }
+        }
+
+        private Dictionary<string, string> words = new Dictionary<string, string>();
+
+        public LocalLanguage(string local_code)
+        {
+            this._local_code = local_code;
+        }
+
+        public string getString(string key)
+        {
+            return words[key];
+        }
+
+        public void setString(string key, string value)
+        {
+            words[key] = value;
+        }
+
+    }
+}
---
+++ Assets/Resources/Scripts/Localization/ThLanguage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chemistrecipe.localization
+{
+    public class ThLanguage : LocalLanguage
+    {
+
+        public ThLanguage() : base("th")
+        {
+            // set text
+        }
+
+    }
+}
